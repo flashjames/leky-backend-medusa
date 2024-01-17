1a38097e6ce0c3c08667e615c9a13a7e90d79742
James Smith <james@leky.hr>
2024-01-17 14:26:24 +0100

    add admin deploy

--- medusa-config.js
+++ medusa-config.js
@@ -148,9 +148,10 @@ const plugins = [
     resolve: "@medusajs/admin",
     /** @type {import('@medusajs/admin').PluginOptions} */
     options: {
+      serve: true,
       autoRebuild: true,
       develop: {
-        open: process.env.OPEN_BROWSER !== "false",
+        open: process.env.OPEN_BROWSER !== "true",
       },
     },
   },
